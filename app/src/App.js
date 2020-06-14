695adcf68d31ecb4b9c08574a0cdf4da51311841
stevekutz <stkutz@gmail.com>
2020-06-13 21:21:11 -0500

    added pages and Router component paths

--- app/src/App.js
+++ app/src/App.js
@@ -1,42 +1,55 @@
 import React from 'react';
-import {Router} from '@reach/router'
+import {Router} from '@reach/router';
 // import logo from './logo.svg';
 import './App.css';
 
+import About from './pages/About';
+import Blog from './pages/Blog';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import Portfolio from './pages/Portfolio';
+
+
 function App() {
-  const navlinks = [
-        {   
-            text: 'Home',
-            path: '/',
-            icon: 'ion-ios-home',
-        },
-        {
-            text: 'Contact',
-            path: '/contact',
-            icon: 'ion-ios-home',
-        },
-        {
-            text: 'About',
-            path: '/about',
-            icon: 'ion-ios-business'
-        },
-        {
-            text: 'Blog',
-            path: '/blog',
-            icon: 'ion-ios-bonfire',
-        },
-        {
-            text: 'Portfolio',
-            path: '/portfolio',
-            icon: 'ion-ios-briefcase',
-        }    
-  ]
+    const navLinks = [
+            {   
+                text: 'Home',
+                path: '/',
+                icon: 'ion-ios-home',
+            },
+            {
+                text: 'Contact',
+                path: '/contact',
+                icon: 'ion-ios-home',
+            },
+            {
+                text: 'About',
+                path: '/about',
+                icon: 'ion-ios-business'
+            },
+            {
+                text: 'Blog',
+                path: '/blog',
+                icon: 'ion-ios-bonfire',
+            },
+            {
+                text: 'Portfolio',
+                path: '/portfolio',
+                icon: 'ion-ios-briefcase',
+            }    
+    ]
   
   
     return (
-    <div className="App">
-
-    </div>
+        <div className="App">
+            <Router>
+                <About path = '/about'/>
+                <Blog path = '/blog' />
+                <Contact path = '/contact' />
+                <Home path = '/home' />
+                <Portfolio path = '/portfolio' />
+            </Router>
+        </div>
   );
 }
 
---
+++ app/src/pages/Blog.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Blog extends React.Component {
+
+    render() {
+
+        return (
+            <div> Blog page </div>
+
+        )
+    }
+
+}
+
+export default Blog
